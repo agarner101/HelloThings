91aa9ad6dee3a007428f2df0c445edd0ac5aa2b1
Andrew Garner <andrewgarner101@gmail.com>
2018-07-31 11:35:09 -0700

    Added project

---
+++ app/src/main/java/io/andrewgarner/hellothings/HomeActivity.java
@@ -0,0 +1,17 @@
+package io.andrewgarner.hellothings;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Skeleton of an Android Things activity.
+ * Created by andrewgarner 7/31/18
+ */
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
